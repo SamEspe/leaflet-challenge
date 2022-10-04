11a01ae30e1adcac2ef988355ae38f9985f3a7f5
Sam Espe <sam.espe.sci@gmail.com>
2022-10-04 18:39:33 -0500

    Created base map. Obtained GeoJSON data.

---
+++ static/js/logic.js
@@ -0,0 +1,23 @@
+// Get URL for GeoJSON
+const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Create Leaflet map object
+let myMap = L.map("map", {
+    center: [40.7128, -74.0059],
+    zoom: 3
+  });
+
+// Add tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+// Bring in data for markers
+d3.json(url).then(function(data){
+    L.geoJson(data).addTo(myMap);
+
+})
+
+// Create marker layer
+
+// 
