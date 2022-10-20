cfab5a963a97af4b0e233e5f762f742c0b3b3d21
Sam Espe <sam.espe.sci@gmail.com>
2022-10-19 19:00:04 -0500

    Extracted metadata

--- static/js/logic.js
+++ static/js/logic.js
@@ -12,11 +12,39 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
+// Style the markers
+// let markerStyle = {
+
+// };
+
+let metadata = [];
+let magnitude = [];
+let place = [];
+let depth = [];
+
 // Bring in data for markers
 d3.json(url).then(function(data){
+    // console.log(data.features.length);
+
+    for (let i = 0; i < data.features.length; i++) {
+        magnitude[i] = [data["features"][i]["properties"]["mag"]];
+        place[i] = [data["features"][i]["properties"]["place"]];
+        depth[i] = [data["features"][i]["geometry"]["coordinates"][2]];
+    };
+
+    console.log("Magnitudes: ", magnitude);
+    console.log("Locations: ", place);
+    console.log("Depths (km): ", depth);
+
+    for (let j = 0; j < data.features.length; j++) {
+        metadata[j] = ("Magnitude: " + String(magnitude[j]) + "Location: " + String(place[j]) + "Depth: " + String(depth[j]));
+    };
+    
+    console.log("Metadata: ", metadata);
+
     L.geoJson(data, {
         pointToLayer: function(feature, latlng){
             return L.circleMarker(latlng)
         }
-    }).addTo(myMap);
+    }).bindPopup(metadata).addTo(myMap);
 })
